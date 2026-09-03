61a6fafafe9eb87378a86d9456b3bccebbf111b4
Siddaroodh venkatapur <siddaroodh2004@gmail.com>
2026-09-03 12:10:28 +0530

    updated resume url

--- src/data/mock.js
+++ src/data/mock.js
@@ -11,7 +11,7 @@ export const personalInfo = {
   leetcode: "https://leetcode.com/u/siddaroodh__2004/",
   hackerrank: "https://www.hackerrank.com/profile/siddaroodh2004",
   profileImage: profilephot,
-  resumeUrl: "https://drive.google.com/file/d/17aTczUqCocRdZXmyB7uu1FABRy5w5kiH/view?usp=sharing",
+  resumeUrl: "https://drive.google.com/file/d/1QnsNhbfr7kmKo7jskii6feOZ__OAahRz/view?usp=sharing",
   about: "Software Engineering graduate (B.Tech 2026) with hands-on experience building scalable backend systems using Java, Spring Boot, Spring Security, and PostgreSQL. Skilled in REST APIs, microservices, authentication, concurrency handling, and transaction management, with strong problem-solving abilities demonstrated through 350+ coding problems across LeetCode, HackerRank, and GeeksforGeeks."
 };
 
